334eeaa66b6d1f2887d41ce0001cdf1bf0d88bd1
MhdMartini <mohamed_martini@student.uml.edu>
2022-04-02 21:05:30 -0400

    added round robin and tested all

--- scheduling.py
+++ scheduling.py
@@ -18,8 +18,7 @@ class Process:
         self.turnaround_time = turnaround_time
 
     def __str__(self) -> str:
-        return f"P{self.pid}\t{self.arrival_time}\t\t{self.burst_time}\t\
-            {self.priority}\t{self.waiting_time}\t\t{self.turnaround_time}"
+        return f"P{self.pid}\t{self.arrival_time}\t\t{self.burst_time}\t\t{self.priority}\t\t{self.waiting_time}\t\t{self.turnaround_time}"
 
 
 class SortedQue:
@@ -73,6 +72,7 @@ class Scheduler:
         return waiting_times, turnaround_times
 
     def print_stats(self):
+        print(self)
         waiting_times, turnaround_times = self.stats
         print("Process\tArrival Time\tBurst Time\tPriority\tWaiting Time\tTurnaround Time")
         for p in self.processes:
@@ -94,6 +94,9 @@ class FCFS(Scheduler):
         if verbose:
             self.print_stats()
 
+    def __str__(self):
+        return "FCFS Scheduling"
+
 
 class SJF(Scheduler):
     """shortest-job-first scheduling"""
@@ -107,6 +110,9 @@ class SJF(Scheduler):
         if verbose:
             self.print_stats()
 
+    def __str__(self):
+        return "SJF Scheduling"
+
 
 class Priority(Scheduler):
     """priority scheduling"""
@@ -119,6 +125,9 @@ class Priority(Scheduler):
         self.stats = self.get_stats()
         if verbose:
             self.print_stats()
+
+    def __str__(self):
+        return "Priority Scheduling"
 
 
 class STCF(Scheduler):
@@ -178,6 +187,41 @@ class STCF(Scheduler):
             if p.arrival_time == t:
                 self.que.enque(p)
 
+    def __str__(self):
+        return "STCF Scheduling"
+
+
+class RoundRobin(Scheduler):
+    def __init__(self, processes: list[Process], verbose: bool = False) -> None:
+        super().__init__(processes)
+        self.que = sorted(processes, key=lambda p: p.arrival_time)
+        self.quantum = 1
+
+        self.schedule()
+        self.stats = self.get_stats()
+        if verbose:
+            self.print_stats()
+
+    def schedule(self):
+        i = 0
+        t = 0
+        while self.que:
+            self.que[i].waiting_time = t
+
+            dt = min(self.quantum, self.que[i].rem_burst_time)
+            self.que[i].rem_burst_time -= dt
+            t += dt
+
+            if self.que[i].rem_burst_time == 0:
+                self.que[i].turnaround_time = t - self.que[i].arrival_time
+                del(self.que[i])
+                i = min(len(self.que) - 1, i)
+                continue
+            i = (i + 1) % len(self.que)
+
+    def __str__(self):
+        return "RR Scheduling"
+
 
 def main(args):
     assert(len(args.burst_times) == len(args.arrival_times))
@@ -192,7 +236,7 @@ def main(args):
                          priority=priorities[i])
                  for i in range(len(args.burst_times))]
 
-    schedulers = [FCFS, SJF, Priority, STCF]
+    schedulers = [FCFS, SJF, Priority, STCF, RoundRobin]
     scheduler = schedulers[args.scheduler]
     scheduler(processes, verbose=True)
 
@@ -201,7 +245,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--scheduler', type=int,
-                        help="0: FCFS, 1: SJF, 2: Priority, 3: STCF")
+                        help="0: FCFS, 1: SJF, 2: Priority, 3: STCF, 4: Round Robin")
     parser.add_argument('--burst_times', type=int, nargs="+",
                         help="expected cpu-burst times of each process")
     parser.add_argument('--arrival_times', type=int, nargs="+",
